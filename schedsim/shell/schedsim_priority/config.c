7c504629d2d85947dbb0c8523a84d94594ee3731
Joel Sherrill <joel.sherrill@oarcorp.com>
2013-04-30 19:06:53 -0500

    schedsim_priority/config.c: Disable use of bsp.h

--- schedsim/shell/schedsim_priority/config.c
+++ schedsim/shell/schedsim_priority/config.c
@@ -3,5 +3,7 @@
 #define CONFIGURE_INIT
 #define CONFIGURE_MAXIMUM_TASKS             1000
 #define CONFIGURE_MAXIMUM_SEMAPHORES        1000
+
+#define CONFIGURE_DISABLE_BSP_SETTINGS
 #include <confdefs.h>
 
