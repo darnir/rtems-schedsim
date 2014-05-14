57629d2f3260588f71311ec6aee1da16e25313a3
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-05-14 08:25:08 -0500

    schedsim_smppriority_affinity: New simulator instance

---
+++ schedsim/shell/schedsim_smppriority_affinity/config.c
@@ -0,0 +1,18 @@
+/**
+ * @file
+ *
+ * Configuration file for Scheduler Simulator using Priority SMP
+ */
+
+#include <rtems.h>
+
+#define CONFIGURE_INIT
+#define CONFIGURE_MAXIMUM_TASKS             1000
+#define CONFIGURE_MAXIMUM_SEMAPHORES        1000
+
+#define CONFIGURE_SMP_APPLICATION
+#define CONFIGURE_SMP_MAXIMUM_PROCESSORS 4
+#define CONFIGURE_SCHEDULER_PRIORITY_AFFINITY_SMP
+
+#define CONFIGURE_DISABLE_BSP_SETTINGS
+#include <rtems/confdefs.h>
