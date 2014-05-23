6425eb90cf4a89db77377b5b470839b82c00cd51
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-05-23 10:56:46 -0500

    commands.c: Include config.h

--- schedsim/shell/shared/commands.c
+++ schedsim/shell/shared/commands.c
@@ -1,13 +1,21 @@
-/*
- *  BASED UPON SOURCE IN RTEMS, MODIFIED FOR SIMULATOR
+/**
+ *  @file
  *
- *  COPYRIGHT (c) 1989-2013.
+ *  BASED UPON SOURCE IN RTEMS, MODIFIED FOR SIMULATOR
+ */
+
+/*
+ *  COPYRIGHT (c) 1989-2014.
  *  On-Line Applications Research Corporation (OAR).
  *
  *  The license and distribution terms for this file may be
  *  found in the file LICENSE in this distribution or at
  *  http://www.rtems.com/license/LICENSE.
  */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 #include "shell.h"
 #include <stdio.h>
