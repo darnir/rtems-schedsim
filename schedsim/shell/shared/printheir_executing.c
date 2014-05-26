968d2b26c7ca5bb61d82254c3d1340b72e35e6b5
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-05-26 13:15:13 -0500

    Unify printing of heir and executing

--- schedsim/shell/shared/printheir_executing.c
+++ schedsim/shell/shared/printheir_executing.c
@@ -1,6 +1,9 @@
+/**
+ * @file
+ * @brief print heir and executing support
+ */
+
 /*
- *  printheir_executing
- *
  *  COPYRIGHT (c) 1989-2013.
  *  On-Line Applications Research Corporation (OAR).
  *
@@ -12,9 +15,17 @@
 #include <stdio.h>
 #include <rtems.h>
 
+#if RTEMS_SMP
+  extern uint32_t Schedsim_Current_cpu;
+  #define MY_CPU Schedsim_Current_cpu
+#else
+  #define MY_CPU 0
+#endif
+
 void PRINT_EXECUTING() {
   printf(
-    "  Thread Executing: 0x%08x priority=%ld\n",
+    "  CPU %d: Thread Executing: 0x%08x priority=%ld\n",
+    MY_CPU,
     _Thread_Executing->Object.id,
     (long) _Thread_Executing->current_priority
   );
@@ -22,7 +33,8 @@ void PRINT_EXECUTING() {
 
 void PRINT_HEIR() {
   printf(
-    "  Thread Heir: 0x%08x priority=%ld\n",
+    "  CPU %d: Thread Heir: 0x%08x priority=%ld\n",
+    MY_CPU,
     _Thread_Heir->Object.id,
     (long) _Thread_Heir->current_priority
   );
