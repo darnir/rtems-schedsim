c19f6a366971bd147bbc61dcee551ac7c579dc8a
Joel Sherrill <joel.sherrill@oarcorp.com>
2014-05-27 10:42:54 -0500

    Add Simulator for Uniprocessor Simple Priority

---
+++ schedsim/shell/schedsim_simple/config.c
@@ -0,0 +1,9 @@
+#include <rtems.h>
+
+#define CONFIGURE_INIT
+#define CONFIGURE_MAXIMUM_TASKS             1000
+#define CONFIGURE_MAXIMUM_SEMAPHORES        1000
+
+#define CONFIGURE_SCHEDULER_SIMPLE
+#define CONFIGURE_DISABLE_BSP_SETTINGS
+#include <confdefs.h>
